111a56b5843562b5d2b54a1520a8a795d565d2a3
a.orlovets <orlovetsjob@gmail.com>
2023-03-03 21:06:00 +0300

    change interface, store

--- src/store/photo.js
+++ src/store/photo.js
@@ -1,31 +1,33 @@
 import { defineStore, acceptHMRUpdate } from 'pinia'
-import { computed, ref } from 'vue'
+// import { computed, ref } from 'vue'
 
-export const usePhotoStore = defineStore('photo', () => {
-  /*  state: () => ({
-   count: 0,
-   name: 'Eduardo',
-   isAdmin: true,
-   items: [],
-   hasChanged: true,
-   }),
-   getters: { doubleCount: state => state.count * 2 },
-   actions: {
-   increment() {
-   this.count++
-   },
-   },*/
-
-  const count = ref(0)
-  const name = ref('Eduardo')
-  const doubleCount = computed(() => count.value * 2)
-
-  function increment() {
-    count.value++
-  }
-
-  return { count, name, doubleCount, increment }
+export const usePhotoStore = defineStore('photo', {
+  state: () => ({
+    count: 0,
+    name: 'Eduardo',
+    isAdmin: true,
+    items: [],
+    hasChanged: true,
+  }),
+  getters: { doubleCount: state => state.count * 2 },
+  actions: {
+    increment() {
+      this.count++
+    },
+  },
 })
+
+/*export const usePhotoStore = defineStore('photo', () => {
+ const count = ref(0)
+ const name = ref('Eduardo')
+ const doubleCount = computed(() => count.value * 2)
+
+ function increment() {
+ count.value++
+ }
+
+ return { count, name, doubleCount, increment }
+ })*/
 
 // Hot module Reload, only for vite
 if (import.meta.hot) {
